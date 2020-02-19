3fa023ef1615e66c73e0f5c30c50441cc1405f13
waltersantos-muniz <walter.ms.santos41@gmail.com>
2020-02-19 09:02:13 -0300

    calc salario

--- src/revisão/Funcionário.java
+++ src/revisão/Funcionário.java
@@ -4,6 +4,25 @@ package revisão;
 public class Funcionário extends Pessoa{
     private int matricula;
     private String senha;
+    private double valorHora;
+
+    public double getValorHora() {
+        return valorHora;
+    }
+
+    public void setValorHora(double valorHora) {
+        this.valorHora = valorHora;
+    }
+    
+    public double calcularSalario(){
+        return this.valorHora*30;
+        
+    }
+    public double calcularSalario(double descontos){
+        descontos = 10;
+        return this.valorHora*30-descontos;
+        
+    }
 
     public int getMatricula() {
         return matricula;
